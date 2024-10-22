2f5d9d0a60bd72e8b8a3b9b31b1601842db7b093
Sarath191181208 <vssarathc04@gmail.com>
2024-10-22 18:20:00 +0530

    refactored cards into indiv comps

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { BsThreeDots } from "react-icons/bs";
 import { AiFillExclamationCircle } from 'react-icons/ai';
 import { FaSignal } from 'react-icons/fa';
-import { Task, TaskPriority } from "../data/Task";
+import { Task, TaskPriority, TaskStatus } from "../data/Task";
+import { priorityKeyFromValue } from '../pages/utils';
 
-export const TaskCard = ({ task }: { task: Task }) => {
-  return (
-    <div className="task-card text-left rounded-lg border">
-      <div className='flex align-center'>
-        <div className="text-sm">{task.id}</div>
-        <UserIcon task={task} className="ml-auto" />
-      </div>
-      <div className="text-md bold">{task.title}</div>
-      <div className='flex align-center gap-1 mt-3'>
-        <TaskCardIcon task={task} />
-        <div>
-          {task.tag.map((tag) => (
-            <TaskTag key={tag} tag={tag} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+interface ChildProps {
+  children: React.ReactNode;
+  className?: string
+}
+
+export const TaskCardContainer = ({ children, className }: ChildProps) => {
+  return <div className={`task-card text-left rounded-lg border ${className}`}>{children}</div>
+}
+
+export const TaskTitle = ({ title }: { title: string }) => {
+  return <div className="text-md bold line-clamp-2">{title}</div>
+};
+
+export const TagsList = ({ tags }: { tags: string[] }) => {
+  return <div>
+    {tags.map((tag) => (
+      <TaskTag key={tag} tag={tag} />
+    ))}
+  </div>
 };
 
 const TaskTag = ({ tag }: { tag: string }) => {
@@ -31,24 +33,35 @@ const TaskTag = ({ tag }: { tag: string }) => {
   </span>;
 };
 
-const TaskCardIcon = ({ task, className }: { task: Task, className?: string }) => {
+export const TaskCardIcon = ({ task, className }: { task: Task, className?: string }) => {
   return <span className={`flex align-center border border-light rounded-md p-1 mr-1 ${className}`}>
-    <PriorityIcon task={task} />
+    <PriorityIcon priority={priorityKeyFromValue(task.priority)} />
   </span>;
 };
 
-const PriorityIcon = ({ task }: { task: Task }) => {
-  const iconMap = {
-    [TaskPriority.NoPriority]: () => <BsThreeDots />,
-    [TaskPriority.Urgent]: () => <AiFillExclamationCircle color="orange" />,
-    [TaskPriority.High]: () => <FaSignal color="gray" />,
-    [TaskPriority.Medium]: () => <FaSignal color="grey" style={{ opacity: 0.6 }} />,
-    [TaskPriority.Low]: () => <FaSignal color="grey" style={{ opacity: 0.3 }} />,
+export const PriorityIcon = ({ priority }: { priority: keyof typeof TaskPriority }) => {
+  const iconMap: Record<keyof typeof TaskPriority, any> = {
+    NoPriority: () => <BsThreeDots />,
+    Urgent: () => <AiFillExclamationCircle color="orange" />,
+    High: () => <FaSignal color="gray" />,
+    Medium: () => <FaSignal color="grey" style={{ opacity: 0.6 }} />,
+    Low: () => <FaSignal color="grey" style={{ opacity: 0.3 }} />,
   };
 
-  return iconMap[task.priority]();
+  return iconMap[priority]();
 };
 
-const UserIcon = ({ task, className }: { task: Task, className: string }) => {
+export const UserIcon = ({ userid, className }: { userid: string, className?: string }) => {
   return <img className={`avatar ${className}`} src={`https://randomuser.me/api/portraits/thumb/men/75.jpg`} alt="avatar" />
+};
+
+export const StatusIcon = ({ status }: { status: TaskStatus }) => {
+  const iconMap: Record<TaskStatus, any> = {
+    Todo: () => <BsThreeDots />,
+    'In progress': () => <BsThreeDots />,
+    Done: () => <BsThreeDots />,
+    Backlog: () => <BsThreeDots />,
+  };
+
+  return iconMap[status]();
 };

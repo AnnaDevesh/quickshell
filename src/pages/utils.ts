922e0a3fd7c5273a3b176ff4859a2e5e7a7be3da
Sarath191181208 <vssarathc04@gmail.com>
2024-10-22 18:49:24 +0530

    sort groupped items util

--- src/pages/utils.ts
+++ src/pages/utils.ts
@@ -1,4 +1,4 @@
-import { GroupingTypes, Task, TaskPriority, TaskPriorityTypes, TaskStatusTypes } from "../data/Task";
+import { GroupingTypes, SortingTypes, Task, TaskPriority, TaskPriorityTypes, TaskStatusTypes } from "../data/Task";
 
 export function priorityKeyFromValue(value: number): keyof typeof TaskPriority {
   const key = TaskPriorityTypes.find((key) => TaskPriority[key] === value);
@@ -39,4 +39,20 @@ export function getBoardColumnKeys(groupby: GroupingTypes, tasks: Task[]) {
     return [...new Set(tasks.map(task => task.userId))];
   }
   throw new Error('Invalid grouping type');
+}
+
+export function sortGrouppedTasks(groupedTasks: { [key: string]: Task[] }, sortby: SortingTypes): Record<any, Task[]> {
+  const newGrouppedTasks: Record<any, Task[]> = {};
+  for (const key in groupedTasks) {
+    newGrouppedTasks[key] = groupedTasks[key].sort((a, b) => {
+      if (sortby === 'Priority') {
+        return a.priority - b.priority;
+      } else if (sortby === 'Title') {
+        return a.title.localeCompare(b.title);
+      }
+      throw new Error('Invalid sorting type');
+    });
+  }
+
+  return newGrouppedTasks;
 }
